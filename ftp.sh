a422ce534e95fa87c4914eec88e0c06d5976cbf6
Aren Clegg <aclegg2011@gmail.com>
2016-12-10 18:42:18 -0800

    reduce code size and use variable arrays

--- ftp.sh
+++ ftp.sh
@@ -1,120 +1,26 @@
 #!/bin/sh
 #configs
-HOST=''
-USER=''
-PASSWD=''
 fullpath=$(pwd)'/out/target/product/'
 rompath=$(pwd)
-ROM_VERSION='ROM-v6.4-'
-
-#Load Config File
+COUNTER=1
 source build_rom.conf
-
 tput reset
 
 
-#d850
-FILE=$fullpath"$device_1"
-cd $FILE
-FILESIZE=$(stat -c%s $ROM_VERSION"$device_1"-*.zip)
-echo "Size of $ROM_VERSION"$device_1"-*.zip = $FILESIZE bytes."
+while [ $COUNTER -lt 4 ]; do
+FILEPATH=$fullpath"${device[COUNTER]}"
+cd $FILEPATH
+FILENAME=$(ls R*.zip)
+FILESIZE=$(stat -c%s R*.zip)
+echo "Size of $FILENAME = $FILESIZE bytes."
 echo ""
 ftp -n -v $HOST <<END_SCRIPT
 user $USER $PASSWD
 binary
 tick
-put $ROM_VERSION"$device_1"-*.zip
+put $FILENAME
 quit
 END_SCRIPT
 cd $rompath
-
-#d851
-FILE=$fullpath"$device_2"
-cd $FILE
-FILESIZE=$(stat -c%s $ROM_VERSION"$device_2"-*.zip)
-echo "Size of $ROM_VERSION"$device_2"-*.zip = $FILESIZE bytes."
-echo ""
-ftp -n -v $HOST <<END_SCRIPT
-user $USER $PASSWD
-binary
-tick
-put $ROM_VERSION"$device_2"-*.zip
-quit
-END_SCRIPT
-cd $rompath
-
-#d852
-FILE=$fullpath"$device_3"
-cd $FILE
-FILESIZE=$(stat -c%s $ROM_VERSION"$device_3"-*.zip)
-echo "Size of $ROM_VERSION"$device_3"-*.zip = $FILESIZE bytes."
-echo ""
-ftp -n -v $HOST <<END_SCRIPT
-user $USER $PASSWD
-binary
-tick
-put $ROM_VERSION"$device_3"-*.zip
-quit
-END_SCRIPT
-cd $rompath
-
-#d855
-FILE=$fullpath"$device_4"
-cd $FILE
-FILESIZE=$(stat -c%s $ROM_VERSION"$device_4"-*.zip)
-echo "Size of $ROM_VERSION"$device_4"-*.zip = $FILESIZE bytes."
-echo ""
-ftp -n -v $HOST <<END_SCRIPT
-user $USER $PASSWD
-binary
-tick
-put $ROM_VERSION"$device_4"-*.zip
-quit
-END_SCRIPT
-cd $rompath
-
-#f400
-FILE=$fullpath"$device_5"
-cd $FILE
-FILESIZE=$(stat -c%s $ROM_VERSION"$device_5"-*.zip)
-echo "Size of $ROM_VERSION"$device_5"-*.zip = $FILESIZE bytes."
-echo ""
-ftp -n -v $HOST <<END_SCRIPT
-user $USER $PASSWD
-binary
-tick
-put $ROM_VERSION"$device_5"-*.zip
-quit
-END_SCRIPT
-cd $rompath
-
-#ls990
-FILE=$fullpath"$device_6"
-cd $FILE
-FILESIZE=$(stat -c%s $ROM_VERSION"$device_6"-*.zip)
-echo "Size of $ROM_VERSION"$device_6"-*.zip = $FILESIZE bytes."
-echo ""
-ftp -n -v $HOST <<END_SCRIPT
-user $USER $PASSWD
-binary
-tick
-put $ROM_VERSION"$device_6"-*.zip
-quit
-END_SCRIPT
-cd $rompath
-
-#vs985
-FILE=$fullpath"$device_7"
-cd $FILE
-FILESIZE=$(stat -c%s $ROM_VERSION"$device_7"-*.zip)
-echo "Size of $ROM_VERSION"$device_7"-*.zip = $FILESIZE bytes."
-echo ""
-ftp -n -v $HOST <<END_SCRIPT
-user $USER $PASSWD
-binary
-tick
-put $ROM_VERSION"$device_7"-*.zip
-quit
-END_SCRIPT
-cd $rompath
-
+let COUNTER=COUNTER+1
+done
